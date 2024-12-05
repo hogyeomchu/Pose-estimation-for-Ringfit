74cf3007138faf38537348301df2aa4addfb73ad
itsme-jung <skydk2000@gmail.com>
2024-12-05 19:55:45 +0900

    modified

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ MSE_function.py = skeleton model 과 input의 x,y좌표를 배열로 인수를 �
 
 put_text.py = 기존 demo.py에 정의된 put_text 함수에 heart beat 아두이노 모듈 값을 불러와 화면에 실시간으로 데이터를 보여주는 부분 추가
 
-
+heart.py = serial 통신을 이용해서 아두이노 센서로 읽은 값을 가져와서 값을 변수에 저장. + 음악 파일 재생
 
 
 # By Hokyeom
--- heart.py
+++ heart.py
@@ -22,16 +22,16 @@ import time
 player = vlc.MediaPlayer("C:/Users/skydk/Desktop/Pose-estimation-for-Ringfit/music.mp3")
 
 # 배속 설정 (1.5배속)
-#player.set_rate(1.5)
+player.set_rate(1.5)
 
 # 음악 재생
 player.play()
 
 # 음악이 끝날 때까지 대기
-#time.sleep(10)  # 음악 길이에 맞게 조정
+time.sleep(100000)  # 음악 길이에 맞게 조정
 
 # 음악 종료
-#player.stop()
+player.stop()
 
 
 
