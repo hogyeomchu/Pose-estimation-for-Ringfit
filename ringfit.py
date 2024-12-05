8ff30a02d872b7e16863362da708a380a9b59035
hogyeomchu <gyeom0808@gmail.com>
2024-12-05 18:23:25 +0900

    Modify

--- ringfit.py
+++ ringfit.py
@@ -15,7 +15,8 @@ sport_list = {
         'maintaining': 70,
         'relaxing': 110,
         'concerned_key_points_idx': [5, 6, 11, 12, 13, 14],
-        'concerned_skeletons_idx': [[14, 12], [15, 13], [6, 12], [7, 13]]
+        'concerned_skeletons_idx': [[14, 12], [15, 13], [6, 12], [7, 13]],
+        'example_idx' : [1] # 여기에 보기 스켈레톤 넣기
     },
     'pushup': {
         'left_points_idx': [6, 8, 10],
@@ -23,7 +24,8 @@ sport_list = {
         'maintaining': 140,
         'relaxing': 120,
         'concerned_key_points_idx': [5, 6, 7, 8, 9, 10],
-        'concerned_skeletons_idx': [[9, 11], [7, 9], [6, 8], [8, 10]]
+        'concerned_skeletons_idx': [[9, 11], [7, 9], [6, 8], [8, 10]],
+        'example_idx' : [1]
     },
     'squat': {
         'left_points_idx': [11, 13, 15],
@@ -31,7 +33,8 @@ sport_list = {
         'maintaining': 80,
         'relaxing': 140,
         'concerned_key_points_idx': [11, 12, 13, 14, 15],
-        'concerned_skeletons_idx': [[16, 14], [14, 12], [17, 15], [15, 13]]
+        'concerned_skeletons_idx': [[16, 14], [14, 12], [17, 15], [15, 13]],
+        'example_idx' : [1]
     }
 }
 
