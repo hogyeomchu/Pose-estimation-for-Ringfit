54bfbec5434091445bddd6f38b2da8abc2410e36
itsme-jung <skydk2000@gmail.com>
2024-12-18 23:11:36 +0900

    timer_interrupt modified

--- timer_test.py
+++ timer_test.py
@@ -1,6 +1,7 @@
 import sched
 import time 
 import cv2 
+import threading 
 
 # 전역 변수 설정
 timer_running = False  # 타이머 실행 여부
@@ -15,9 +16,9 @@ def countdown_timer():
     if remaining_time > 0:
         print("time left: ", remaining_time)
         remaining_time -= 1  # 남은 시간 1초 감소
-        scheduler.enter(1, 1, countdown_timer)  # 1초 후에 다시 실행 (delay, priority, 호출 함수)
+        threading.Timer(1, countdown_timer).start()  # 1초 후에 다시 실행
     else:
-        # timer =0 이 됐을 떄 소리가 나게 한다. 
+        print("timer 종료!") # timer =0 이 됐을 떄 소리가 나게 한다. 
         timer_running = False  # 타이머 종료
 
 
@@ -27,12 +28,7 @@ def start_timer(duration):
     remaining_time = duration  # 초기화
     if not timer_running:
         timer_running = True
-        scheduler.enter(1, 1, countdown_timer)  # 타이머 시작
-
-
-
-
-
+        countdown_timer()
 
 
 
@@ -43,8 +39,9 @@ def start_timer(duration):
 def main():
     global remaining_time
     start_timer(3)
-    scheduler.run()  # 스케줄러를 실행하여 타이머를 동작시킴
-
+    while timer_running:
+        print("타이머 작동 중...")
+        time.sleep(0.5)
 
     
 if __name__ == "__main__":
