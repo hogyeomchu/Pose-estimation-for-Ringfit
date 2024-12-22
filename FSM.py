2052a89e75b2bd71294e0b288c7d8c69375d03e4
itsme-jung <skydk2000@gmail.com>
2024-12-22 23:31:50 +0900

    ff

--- FSM.py
+++ FSM.py
@@ -27,15 +27,15 @@ class FSM:
     def __init__(self):
         # 상태 초기화
         self.states = {  
-            "ready": State(delay=5, nstate1="start", nstate2="start", nstate3="ready"),
-            "start": State(delay=3, nstate1="redo", nstate2="redo", nstate3="ready"),
-            "redo": State(delay=3, nstate1="start", nstate2="finish", nstate3="ready"),
-            "finish": State(delay=0, nstate1="ready", nstate2="ready", nstate3="finish"),
+            "ready": State(delay=5, nstate1="start", nstate2="start"),
+            "start": State(delay=3, nstate1="redo", nstate2="redo"),
+            "redo": State(delay=3, nstate1="start", nstate2="finish"),
+            "finish": State(delay=0, nstate1="ready", nstate2="ready"),
 
         }
         self.current_state = "ready"  # 초기 상태 설정
 
-    def transition(self, condition1, condition2, condition3):   #  timer == 0 / timer == 0 & count==10 / Key interrupt
+    def transition(self, condition1, condition2, condition3):   #  timer == 0 / timer == 0 & count==10
         # 현재 상태의 객체 가져오기
         state_obj = self.states[self.current_state]
 
@@ -44,8 +44,6 @@ class FSM:
             self.current_state = state_obj.nstate1
         if condition2 == 1 and state_obj.nstate2:
             self.current_state = state_obj.nstate2
-        if condition3 == 1 and state_obj.nstate3:
-            self.current_state = state_obj.nstate3
         else:
             print("유효하지 않은 조건입니다.")
 
